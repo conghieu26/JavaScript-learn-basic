14d1608b0b8f5e32b6431f2ff6328f6a5bbd389f
conghieu26 <hieunc00@gmail.com>
2023-12-11 19:08:08 +0700

    Finished Ex 12.

---
+++ Exercise-solutions/Exercise-12/12w.js
@@ -0,0 +1,16 @@
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  } else if (event.key === 'a') {
+    autoPlay();
+  
+  // Add an if-statement condition to
+  // check if 'Backspace' was pressed.
+  } else if (event.key === 'Backspace') {
+    resetScore();
+  }
+});
